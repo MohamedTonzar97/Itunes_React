4e789abdda4d468a50bf9e54c81d6cad34ac6e51
SimoTonzar <mohamedtonzar@gmail.com>
2021-03-23 00:17:30 +0100

    FInal

--- src/itunesService.js
+++ src/itunesService.js
@@ -1,5 +1,5 @@
 export async function  fetchItunesSongs(term){
-	var API = 'http://localhost:8085/itunes';
+	var API = 'https://serveritunes.herokuapp.com/itunes';
 	try {
 		var url = `${API}?term=${term}`;
 		var response = await fetch(url);
